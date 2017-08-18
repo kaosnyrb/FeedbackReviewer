12f6aead7c38deeeb505e963b116448266abf236
Bryn Stringer <BStringer@intelligentenvironments.com>
2017-08-18 16:00:36 +0100

    Added local DB File and EmployeeDataSerivce. Normally you don't commit MDF's but I want this to work off the bat.

--- FeedbackReviewer/Services/EmployeeDataService.cs
+++ FeedbackReviewer/Services/EmployeeDataService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using FeedbackReviewer.Models;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace FeedbackReviewer.Services
 {
@@ -10,27 +12,101 @@ namespace FeedbackReviewer.Services
     {
         public Employee AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"INSERT INTO [Employees]([Id],[Name]) VALUES(@Id,@Name)";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = employee.EmployeeId });
+                    command.Parameters.Add(new SqlParameter("Name", System.Data.SqlDbType.NVarChar) { Value = employee.Name});
+                    command.ExecuteNonQuery();
+                }
+                myConnection.Close();
+            }
+            return employee;
         }
 
         public void DeleteEmployee(Guid employeeId)
         {
-            throw new NotImplementedException();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"DELETE FROM [Employees] WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = employeeId });
+                    command.ExecuteNonQuery();
+                }
+                myConnection.Close();
+            }
         }
 
         public List<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            var employeeList = new List<Employee>();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"SELECT * FROM [Employees]";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    var myReader = command.ExecuteReader();
+                    while (myReader.Read())
+                    {
+                        Employee temp = new Employee()
+                        {
+                            EmployeeId = Guid.Parse(myReader["Id"].ToString()),
+                            Name = myReader["Name"].ToString()
+                        };
+                        employeeList.Add(temp);
+                    }
+                }
+                myConnection.Close();
+            }
+            return employeeList;
         }
 
         public Employee GetEmployee(Guid employeeId)
         {
-            throw new NotImplementedException();
+            Employee employee = null;
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"SELECT * FROM [Employees] WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = employeeId });
+                    var myReader = command.ExecuteReader();
+                    while (myReader.Read())
+                    {
+                        employee = new Employee()
+                        {
+                            EmployeeId = Guid.Parse(myReader["Id"].ToString()),
+                            Name = myReader["Name"].ToString()
+                        };
+                    }
+                }
+                myConnection.Close();
+            }
+            return employee;
         }
 
         public Employee UpdateEmployee(Guid employeeId, Employee employee)
         {
-            throw new NotImplementedException();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"UPDATE [Employees] SET [Name] = @Name WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = employee.EmployeeId });
+                    command.Parameters.Add(new SqlParameter("Name", System.Data.SqlDbType.NVarChar) { Value = employee.Name });
+                    command.ExecuteNonQuery();
+                }
+                myConnection.Close();
+            }
+            return employee;
         }
     }
 }
