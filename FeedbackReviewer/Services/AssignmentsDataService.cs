293a82a3e14d40f7e602e58a21b8ae497babc1ac
Bryn Stringer <BStringer@intelligentenvironments.com>
2017-08-18 16:20:49 +0100

    Completed Performance and Assignments Data Services.

--- FeedbackReviewer/Services/AssignmentsDataService.cs
+++ FeedbackReviewer/Services/AssignmentsDataService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using FeedbackReviewer.Models;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace FeedbackReviewer.Services
 {
@@ -10,17 +12,74 @@ namespace FeedbackReviewer.Services
     {
         public Assignment AddAssignment(Assignment assignment)
         {
-            throw new NotImplementedException();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"INSERT INTO [Assignments]([Id],[PerformanceReviewId],[EmployeeId]) VALUES(@Id,@PerformanceReviewId,@EmployeeId)";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = assignment.AssignmentId });
+                    command.Parameters.Add(new SqlParameter("PerformanceReviewId", System.Data.SqlDbType.UniqueIdentifier) { Value = assignment.PerformanceReviewId });
+                    command.Parameters.Add(new SqlParameter("EmployeeId", System.Data.SqlDbType.UniqueIdentifier) { Value = assignment.EmployeeId });
+
+                    command.ExecuteNonQuery();
+                }
+                myConnection.Close();
+            }
+            return assignment;
         }
 
         public Assignment GetAssignment(Guid assignmentId)
         {
-            throw new NotImplementedException();
+            Assignment assignment = null;
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"SELECT * FROM [Assignments] WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = assignmentId });
+                    var myReader = command.ExecuteReader();
+                    while (myReader.Read())
+                    {
+                        assignment = new Assignment()
+                        {
+                            AssignmentId = Guid.Parse(myReader["Id"].ToString()),
+                            PerformanceReviewId = Guid.Parse(myReader["PerformanceReviewId"].ToString()),
+                            EmployeeId = Guid.Parse(myReader["EmployeeId"].ToString()),
+                        };
+                    }
+                }
+                myConnection.Close();
+            }
+            return assignment;
         }
 
         public List<Assignment> GetEmployeesAssignments(Guid employeeId)
         {
-            throw new NotImplementedException();
+            List<Assignment> assignments = new List<Assignment>();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"SELECT * FROM [Assignments] WHERE EmployeeId = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = employeeId });
+                    var myReader = command.ExecuteReader();
+                    while (myReader.Read())
+                    {
+                        var assignment = new Assignment()
+                        {
+                            AssignmentId = Guid.Parse(myReader["Id"].ToString()),
+                            PerformanceReviewId = Guid.Parse(myReader["PerformanceReviewId"].ToString()),
+                            EmployeeId = Guid.Parse(myReader["EmployeeId"].ToString()),
+                        };
+                        assignments.Add(assignment);
+                    }
+                }
+                myConnection.Close();
+            }
+            return assignments;
         }
     }
 }
--- FeedbackReviewer/Services/PerformanceReviewDataService.cs
+++ FeedbackReviewer/Services/PerformanceReviewDataService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using FeedbackReviewer.Models;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace FeedbackReviewer.Services
 {
@@ -10,22 +12,91 @@ namespace FeedbackReviewer.Services
     {
         public PerformanceReview AddPerformanceReview(PerformanceReview performanceReview)
         {
-            throw new NotImplementedException();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"INSERT INTO [PerformanceReviews]([Id],[EmployeeId],[Feedback]) VALUES(@Id,@EmployeeId,@Feedback)";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = performanceReview.PerformanceReviewId });
+                    command.Parameters.Add(new SqlParameter("EmployeeId", System.Data.SqlDbType.UniqueIdentifier) { Value = performanceReview.EmployeeId });
+                    command.Parameters.Add(new SqlParameter("Feedback", System.Data.SqlDbType.NVarChar) { Value = performanceReview.Feedback });
+                    command.ExecuteNonQuery();
+                }
+                myConnection.Close();
+            }
+            return performanceReview;
         }
 
         public List<PerformanceReview> GetAllPerformanceReviews()
         {
-            throw new NotImplementedException();
+            var performanceReviewList = new List<PerformanceReview>();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"SELECT * FROM [PerformanceReviews]";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    var myReader = command.ExecuteReader();
+                    while (myReader.Read())
+                    {
+                        PerformanceReview temp = new PerformanceReview()
+                        {
+                            PerformanceReviewId = Guid.Parse(myReader["Id"].ToString()),
+                            EmployeeId = Guid.Parse(myReader["EmployeeId"].ToString()),
+                            Feedback = myReader["Feedback"].ToString(),
+                        };
+                        performanceReviewList.Add(temp);
+                    }
+                }
+                myConnection.Close();
+            }
+            return performanceReviewList;
         }
 
         public PerformanceReview GetPerformanceReview(Guid performanceReviewId)
         {
-            throw new NotImplementedException();
+            PerformanceReview performanceReview = null;
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"SELECT * FROM [PerformanceReviews] WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = performanceReviewId });
+                    var myReader = command.ExecuteReader();
+                    while (myReader.Read())
+                    {
+                        performanceReview = new PerformanceReview()
+                        {
+                            PerformanceReviewId = Guid.Parse(myReader["Id"].ToString()),
+                            EmployeeId = Guid.Parse(myReader["EmployeeId"].ToString()),
+                            Feedback = myReader["Feedback"].ToString(),
+                        };
+                    }
+                }
+                myConnection.Close();
+            }
+            return performanceReview;
         }
 
         public PerformanceReview UpdatePerformanceReview(Guid performanceReviewId, PerformanceReview performanceReview)
         {
-            throw new NotImplementedException();
+            using (var myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["FeedbackReviewerDB"].ConnectionString))
+            {
+                myConnection.Open();
+                string sqlCommand = @"UPDATE [PerformanceReviews] SET [EmployeeId] = @EmployeeId, [Feedback] = @Feedback WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(sqlCommand, myConnection))
+                {
+                    command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier) { Value = performanceReviewId });
+                    command.Parameters.Add(new SqlParameter("EmployeeId", System.Data.SqlDbType.UniqueIdentifier) { Value = performanceReview.EmployeeId });
+                    command.Parameters.Add(new SqlParameter("Feedback", System.Data.SqlDbType.NVarChar) { Value = performanceReview.Feedback });
+
+                    command.ExecuteNonQuery();
+                }
+                myConnection.Close();
+            }
+            return performanceReview;
         }
     }
 }
